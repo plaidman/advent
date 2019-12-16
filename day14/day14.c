30d241089dd328440641e814d2044891437e1701
Tomsic, Jason <jrtomsic@gmail.com>
2019-12-16 15:47:15 -0500

    day 14 part 2 and improved part 1

--- day14/day14.c
+++ day14/day14.c
@@ -4,6 +4,7 @@
 #include <regex.h>
 #include <math.h>
 
+#define llu long long unsigned int
 #define Element struct Element
 #define RecipeElement struct RecipeElement
 
@@ -11,10 +12,9 @@ Element {
     char* label;
     int quantity;
     char* recipeString;
-    int separation;
     RecipeElement* recipeHead;
     Element* next;
-    int reserves;
+    llu reserves;
 };
 
 RecipeElement {
@@ -31,14 +31,9 @@ char* substring(char*, regmatch_t);
 Element* createElement(char*, int, char*);
 RecipeElement* createRecipeElement(char*, int, Element*);
 Element* findElement(Element*, char*);
-RecipeElement* findRecipeElement(RecipeElement*, char*);
 void parseRecipe(Element*, Element*);
-void calculateSeparation(Element*);
-void breakdownIngredient(Element*);
-RecipeElement* findMaxSeparatedRecipeElement(RecipeElement*);
-RecipeElement* findLastRecipeElement(RecipeElement*);
-void removeRecipeElement(RecipeElement**, RecipeElement*);
-void normalizeRecipe(Element*);
+int refineElement(Element*, llu, int);
+void printIndent(int);
 
 int main(int argc, char const *argv[]) {
     Element* head = readInput("input.txt");
@@ -49,132 +44,74 @@ int main(int argc, char const *argv[]) {
         current = current->next;
     }
 
-    current = head;
-    while (current != NULL) {
-        calculateSeparation(current);
-        current = current->next;
-    }
-
-    // current = head;
-    // while (current != NULL) {
-    //     printElement(current);
-    //     current = current->next;
-    // }
-
     printf("\n-----------------------------------------\n\n");
 
     Element* fuel = findElement(head, "FUEL");
-    while (fuel->separation > 1) {
-        breakdownIngredient(fuel);
-        normalizeRecipe(fuel);
-        fuel->separation = -1;
-        calculateSeparation(fuel);
-    }
+    refineElement(fuel, 1, 0);
+    llu count = 1000000000000 - head->reserves;
+    printf("%llu ore used for 1 fuel\n", count);
 
-    int quantity = fuel->recipeHead->quantity;
-    printf("ore for 1 fuel: %d\n", quantity);
+    printf("\n-----------------------------------------\n\n");
+
+    count = 1;
+    while (refineElement(fuel, 1, 0) != 0) {
+        if (count % 10000 == 0) printf("%llu ORE left\n", head->reserves);
+        count++;
+    }
+    printf("\n%llu units of fuel refined\n", count);
 
     exit(0);
 }
 
-void normalizeRecipe(Element* element) {
-    printf("normalize recipe\n");
+void printIndent(int indent) {
+    for (int i = 0; i < indent; i++) {
+        printf("  ");
+    }
+}
 
-    RecipeElement* newCurr = element->recipeHead;
-    RecipeElement* oldCurr;
+int refineElement(Element* element, llu quantity, int indentation) {
+    // printIndent(indentation);
+    // printf("refining %llu %s\n", quantity, element->label);
 
-    while (newCurr != NULL) {
-        printf("newCurr %s\n", newCurr->label);
-        oldCurr = newCurr->next;
+    if (element->reserves >= quantity) {
+        element->reserves -= quantity;
+        // printIndent(indentation);
+        // printf("using %llu %s reserves, %llu remaining\n", quantity, element->label, element->reserves);
+        return 1;
+    }
 
-        while (oldCurr != NULL) {
-            printf("oldCurr %s\n", oldCurr->label);
+    if (strcmp(element->label, "ORE") == 0) {
+        // printIndent(indentation);
+        // printf("only have %llu ORE, need %llu, not enough\n", element->reserves, quantity);
+        return 0;
+    }
 
-            if (strcmp(newCurr->label, oldCurr->label) == 0) {
-                printf("normalizing %s\n", newCurr->label);
-                newCurr->quantity += oldCurr->quantity;
-                removeRecipeElement(&(element->recipeHead), oldCurr);
-    
-                oldCurr = newCurr->next;
-            } else {
-                oldCurr = oldCurr->next;
+    while (element->reserves < quantity) {
+        // printIndent(indentation);
+        // printf("have %llu %s, need %llu, not enough\n", element->reserves, element->label, quantity);
+        RecipeElement* current = element->recipeHead;
+
+        // refine it
+        while (current != NULL) {
+            int result = refineElement(current->link, current->quantity, indentation+1);
+            if (result == 0) {
+                // printIndent(indentation);
+                // printf("only have %llu %s, need %llu, not enough\n", element->reserves, element->label, quantity);
+                return 0;
             }
+
+            current = current->next;
         }
 
-        newCurr = newCurr->next;
+        // printIndent(indentation);
+        // printf("finished refining %d %s\n", element->quantity, element->label);
+        element->reserves += element->quantity;
     }
 
-    printElement(element);
-}
-
-void breakdownIngredient(Element* element) {
-    printf("breakdown ingredient\n");
-
-    RecipeElement* maxSep = findMaxSeparatedRecipeElement(element->recipeHead);
-    removeRecipeElement(&(element->recipeHead), maxSep);
-
-    int multiplier = ceil(maxSep->quantity * 1.00 / maxSep->link->quantity);
-    printf("multiplier: %d for %s\n", multiplier, maxSep->label);
-
-    RecipeElement* tail = findLastRecipeElement(element->recipeHead);
-    RecipeElement* current = maxSep->link->recipeHead;
-    while (current != NULL) {
-        tail->next = createRecipeElement(current->label, current->quantity * multiplier, current->link);
-        tail = tail->next;
-        current = current->next;
-    }
-
-    printElement(element);
-}
-
-void removeRecipeElement(RecipeElement** head, RecipeElement* target) {
-    // printf("searching for %d\n", (int)target);
-    if (*head == target) {
-        // printf("matches head %d\n", (int)*head);
-        *head = (*head)->next;
-        return;
-    }
-
-    RecipeElement* current = *head;
-    while (current->next != NULL) {
-        // printf("looking at %d\n", (int)current->next);
-        if (current->next == target) {
-            // printf("matches\n");
-            current->next = target->next;
-            target->next = NULL;
-            return;
-        }
-
-        current = current->next;
-    }
-
-    printf("unable to remove recipe element: %s\n", target->label);
-    exit(1);
-}
-
-void calculateSeparation(Element* element) {
-    printf("calculate separation\n");
-
-    if (element->separation != -1) return;
-
-    RecipeElement* current = element->recipeHead;
-    int maxSeparation = 0;
-
-    while (current != NULL) {
-        if (current->link->separation == -1) {
-            calculateSeparation(current->link);
-        }
-
-        if (current->link->separation > maxSeparation) {
-            maxSeparation = current->link->separation;
-        }
-
-        current = current->next;
-    }
-
-    element->separation = maxSeparation + 1;
-
-    printElement(element);
+    element->reserves -= quantity;
+    // printIndent(indentation);
+    // printf("using %llu %s reserves, %llu remaining\n", quantity, element->label, element->reserves);
+    return 1;
 }
 
 void parseRecipe(Element* element, Element* head) {
@@ -203,51 +140,6 @@ void parseRecipe(Element* element, Element* head) {
     element->recipeHead = recipeHead->next;
 }
 
-RecipeElement* findLastRecipeElement(RecipeElement* head) {
-    RecipeElement* current = head;
-
-    while (current != NULL) {
-        if (current->next == NULL) {
-            return current;
-        }
-
-        current = current->next;
-    }
-
-    printf("tail recipe element not found\n");
-    exit(1);
-}
-
-RecipeElement* findMaxSeparatedRecipeElement(RecipeElement* head) {
-    RecipeElement* current = head;
-    RecipeElement* max = head;
-
-    while (current != NULL) {
-        if (max->link->separation < current->link->separation) {
-            max = current;
-        }
-
-        current = current->next;
-    }
-
-    return max;
-}
-
-RecipeElement* findRecipeElement(RecipeElement* head, char* label) {
-    RecipeElement* current = head;
-
-    while (current != NULL) {
-        if (strcmp(current->label, label) == 0) {
-            return current;
-        }
-
-        current = current->next;
-    }
-
-    printf("recipe element not found: %s\n", label);
-    exit(1);
-}
-
 Element* findElement(Element* head, char* label) {
     Element* current = head;
 
@@ -265,7 +157,7 @@ Element* findElement(Element* head, char* label) {
 
 Element* readInput(char* filename) {
     Element* head = createElement("ORE", 0, "");
-    head->separation = 0;
+    head->reserves = 1000000000000;
     Element* current = head;
 
     FILE* file = fopen(filename, "r");
@@ -329,7 +221,6 @@ Element* createElement(char* label, int quantity, char* recipe) {
     element->recipeString = recipe;
 
     element->reserves = 0;
-    element->separation = -1;
     element->next = NULL;
     element->recipeHead = NULL;
 
@@ -337,7 +228,7 @@ Element* createElement(char* label, int quantity, char* recipe) {
 }
 
 void printElement(Element* element) {
-    printf("%d * %s: (%d)\n", element->quantity, element->label, element->separation);
+    printf("%d * %s: (%llu)\n", element->quantity, element->label, element->reserves);
     RecipeElement* current = element->recipeHead;
 
     while (current != NULL) {
