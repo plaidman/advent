22b082fbe1a44cb4ff4041865303174ffbf194ab
Tomsic, Jason <jrtomsic@gmail.com>
2019-12-16 14:18:01 -0500

    answer for day 14 part 1

--- day14/day14.c
+++ day14/day14.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <regex.h>
+#include <math.h>
 
 #define Element struct Element
 #define RecipeElement struct RecipeElement
@@ -13,6 +14,7 @@ Element {
     int separation;
     RecipeElement* recipeHead;
     Element* next;
+    int reserves;
 };
 
 RecipeElement {
@@ -29,8 +31,14 @@ char* substring(char*, regmatch_t);
 Element* createElement(char*, int, char*);
 RecipeElement* createRecipeElement(char*, int, Element*);
 Element* findElement(Element*, char*);
+RecipeElement* findRecipeElement(RecipeElement*, char*);
 void parseRecipe(Element*, Element*);
 void calculateSeparation(Element*);
+void breakdownIngredient(Element*);
+RecipeElement* findMaxSeparatedRecipeElement(RecipeElement*);
+RecipeElement* findLastRecipeElement(RecipeElement*);
+void removeRecipeElement(RecipeElement**, RecipeElement*);
+void normalizeRecipe(Element*);
 
 int main(int argc, char const *argv[]) {
     Element* head = readInput("input.txt");
@@ -47,22 +55,106 @@ int main(int argc, char const *argv[]) {
         current = current->next;
     }
 
-    current = head;
-    while (current != NULL) {
-        printElement(current);
-        current = current->next;
-    }
-
     // current = head;
     // while (current != NULL) {
-    //     calculateSeparation(current);
+    //     printElement(current);
     //     current = current->next;
     // }
+
+    printf("\n-----------------------------------------\n\n");
+
+    Element* fuel = findElement(head, "FUEL");
+    while (fuel->separation > 1) {
+        breakdownIngredient(fuel);
+        normalizeRecipe(fuel);
+        fuel->separation = -1;
+        calculateSeparation(fuel);
+    }
+
+    int quantity = fuel->recipeHead->quantity;
+    printf("ore for 1 fuel: %d\n", quantity);
 
     exit(0);
 }
 
+void normalizeRecipe(Element* element) {
+    printf("normalize recipe\n");
+
+    RecipeElement* newCurr = element->recipeHead;
+    RecipeElement* oldCurr;
+
+    while (newCurr != NULL) {
+        printf("newCurr %s\n", newCurr->label);
+        oldCurr = newCurr->next;
+
+        while (oldCurr != NULL) {
+            printf("oldCurr %s\n", oldCurr->label);
+
+            if (strcmp(newCurr->label, oldCurr->label) == 0) {
+                printf("normalizing %s\n", newCurr->label);
+                newCurr->quantity += oldCurr->quantity;
+                removeRecipeElement(&(element->recipeHead), oldCurr);
+    
+                oldCurr = newCurr->next;
+            } else {
+                oldCurr = oldCurr->next;
+            }
+        }
+
+        newCurr = newCurr->next;
+    }
+
+    printElement(element);
+}
+
+void breakdownIngredient(Element* element) {
+    printf("breakdown ingredient\n");
+
+    RecipeElement* maxSep = findMaxSeparatedRecipeElement(element->recipeHead);
+    removeRecipeElement(&(element->recipeHead), maxSep);
+
+    int multiplier = ceil(maxSep->quantity * 1.00 / maxSep->link->quantity);
+    printf("multiplier: %d for %s\n", multiplier, maxSep->label);
+
+    RecipeElement* tail = findLastRecipeElement(element->recipeHead);
+    RecipeElement* current = maxSep->link->recipeHead;
+    while (current != NULL) {
+        tail->next = createRecipeElement(current->label, current->quantity * multiplier, current->link);
+        tail = tail->next;
+        current = current->next;
+    }
+
+    printElement(element);
+}
+
+void removeRecipeElement(RecipeElement** head, RecipeElement* target) {
+    // printf("searching for %d\n", (int)target);
+    if (*head == target) {
+        // printf("matches head %d\n", (int)*head);
+        *head = (*head)->next;
+        return;
+    }
+
+    RecipeElement* current = *head;
+    while (current->next != NULL) {
+        // printf("looking at %d\n", (int)current->next);
+        if (current->next == target) {
+            // printf("matches\n");
+            current->next = target->next;
+            target->next = NULL;
+            return;
+        }
+
+        current = current->next;
+    }
+
+    printf("unable to remove recipe element: %s\n", target->label);
+    exit(1);
+}
+
 void calculateSeparation(Element* element) {
+    printf("calculate separation\n");
+
     if (element->separation != -1) return;
 
     RecipeElement* current = element->recipeHead;
@@ -81,6 +173,8 @@ void calculateSeparation(Element* element) {
     }
 
     element->separation = maxSeparation + 1;
+
+    printElement(element);
 }
 
 void parseRecipe(Element* element, Element* head) {
@@ -107,6 +201,51 @@ void parseRecipe(Element* element, Element* head) {
     }
 
     element->recipeHead = recipeHead->next;
+}
+
+RecipeElement* findLastRecipeElement(RecipeElement* head) {
+    RecipeElement* current = head;
+
+    while (current != NULL) {
+        if (current->next == NULL) {
+            return current;
+        }
+
+        current = current->next;
+    }
+
+    printf("tail recipe element not found\n");
+    exit(1);
+}
+
+RecipeElement* findMaxSeparatedRecipeElement(RecipeElement* head) {
+    RecipeElement* current = head;
+    RecipeElement* max = head;
+
+    while (current != NULL) {
+        if (max->link->separation < current->link->separation) {
+            max = current;
+        }
+
+        current = current->next;
+    }
+
+    return max;
+}
+
+RecipeElement* findRecipeElement(RecipeElement* head, char* label) {
+    RecipeElement* current = head;
+
+    while (current != NULL) {
+        if (strcmp(current->label, label) == 0) {
+            return current;
+        }
+
+        current = current->next;
+    }
+
+    printf("recipe element not found: %s\n", label);
+    exit(1);
 }
 
 Element* findElement(Element* head, char* label) {
@@ -189,6 +328,7 @@ Element* createElement(char* label, int quantity, char* recipe) {
     element->quantity = quantity;
     element->recipeString = recipe;
 
+    element->reserves = 0;
     element->separation = -1;
     element->next = NULL;
     element->recipeHead = NULL;
