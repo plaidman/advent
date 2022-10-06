66e5bf2542940ab0d3250f00a54ba2d0f718f6e2
Jason Tomsic <jasontomsic@quickenloans.com>
2022-10-06 15:33:51 -0400

    day 12 part 2

--- 2021-rust/rust-12/src/main.rs
+++ 2021-rust/rust-12/src/main.rs
@@ -12,27 +12,32 @@ fn main() {
         generate_map(path, &mut map);
     }
     
-    let sum = traverse_caves("start".to_string(), &map, vec![]);
+    // change last param to true for part-one
+    let sum = traverse_caves("start".to_string(), &map, vec![], false);
     println!("{}", sum);
 
     println!("done");
 }
 
-fn traverse_caves(label: String, map: &HashMap<String, Cave>, mut path: Vec<String>) -> u32 {
+fn traverse_caves(label: String, map: &HashMap<String, Cave>, mut path: Vec<String>, mut small_twice: bool) -> u32 {
     if label == "end" {
         return 1;
     }
-    
+
     let cave = map.get(&label).unwrap();
     if !cave.is_large && path.contains(&label) {
-        return 0;
+        if small_twice {
+            return 0;
+        }
+
+        small_twice = true;
     }
     
     path.push(label);
     
     let mut sum = 0;
     for branch in cave.paths.as_slice() {
-        sum += traverse_caves(branch.to_string(), &map, path.clone());
+        sum += traverse_caves(branch.to_string(), &map, path.clone(), small_twice);
     }
 
     return sum;
@@ -49,11 +54,15 @@ fn generate_map(path: String, map: &mut HashMap<String, Cave>) {
         map.insert(right.to_string(), Cave::new(right.to_string()));
     }
     
-    let left_cave = map.get_mut(&left.to_string()).unwrap();
-    left_cave.paths.push(right.to_string());
+    if right != "start".to_string() {
+        let left_cave = map.get_mut(&left.to_string()).unwrap();
+        left_cave.paths.push(right.to_string());
+    }
 
-    let right_cave = map.get_mut(&right.to_string()).unwrap();
-    right_cave.paths.push(left.to_string());
+    if left != "start".to_string() {
+        let right_cave = map.get_mut(&right.to_string()).unwrap();
+        right_cave.paths.push(left.to_string());
+    }
     
     return;
 }
